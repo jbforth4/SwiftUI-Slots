f47ef66730ab84cdc5e12b2a863c7cd2b2787e35
Jared Forth <jared.forth@maritz.com>
2020-07-01 14:16:21 -0500

    initial commit

--- Slots Demo/ContentView.swift
+++ Slots Demo/ContentView.swift
@@ -9,8 +9,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var symbols = ["apple", "donut", "lemon"]
+    @State private var numbers = [0, 0, 0]
+    @State private var credits = 1000
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Rectangle().foregroundColor(Color(
+                red: 200/255, green: 143/255, blue: 32/255
+            )).edgesIgnoringSafeArea(.all)
+            
+            Rectangle().foregroundColor(Color(
+                red: 228/255, green: 195/255, blue: 76/255
+            )).rotationEffect(Angle(degrees: 45)).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Image(systemName: "star.fill").foregroundColor(.yellow)
+                    Text("SwiftUI Slots").bold().foregroundColor(.white)
+                    Image(systemName: "star.fill").foregroundColor(.yellow)
+                }.scaleEffect(2)
+                Spacer()
+                Text("Credits: 1000")
+                    .foregroundColor(.black)
+                    .padding(.all, 10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(symbols[numbers[0]])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    
+                    Image(symbols[numbers[1]])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    
+                    Image(symbols[numbers[2]])
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    self.numbers[0] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[1] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
+                }) {
+                    Text("Spin")
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(.all, 10)
+                        .padding([.leading, .trailing])
+                        .background(Color.pink)
+                        .cornerRadius(20)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
