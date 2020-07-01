844cb4ae4e83226563eeb5edd0ff980126508fb8
Jared Forth <jared.forth@maritz.com>
2020-07-01 09:58:06 -0500

    Initial Commit

---
+++ Slots Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slots Demo
+//
+//  Created by Forth, Jared on 7/1/20.
+//  Copyright © 2020 Forth, Jared. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
